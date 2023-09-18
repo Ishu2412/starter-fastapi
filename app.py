73eba9d9f0a432483bb47c5df37b23dc133b2f7e
Shrayash Shukla <98635559+Ishu2412@users.noreply.github.com>
2023-09-19 02:03:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,50 @@
+import tensorflow as tf
+import cv2
+import numpy as np
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
-
-from pydantic import BaseModel
+import skimage
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    item_id: int
+label = ['Seborrheic Keratoses and other Benign Tumors',
+ 'Psoriasis pictures Lichen Planus and related diseases',
+ 'Tinea Ringworm Candidiasis and other Fungal Infections',
+ 'Eczema Photos',
+ 'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions']
 
+vgg_model = tf.keras.applications.vgg19.VGG19(weights = 'imagenet',  include_top = False, input_shape = (180, 180, 3))
+
+# Load saved model
+model = tf.keras.models.load_model('skin.h5')
+
+def predict_skin_disease(image_path):
+   # Define list of class names
+    class_names = label
+
+    print(image_path)
+    img = skimage.io.imread( image_path )
+    img = cv2.resize(img, (180, 180))
+    img = np.array(img)
+    img = np.expand_dims(img, axis=0)
+    img = vgg_model.predict(img)
+    img = img.reshape(1, -1)
+
+    # Make prediction on preprocessed image
+    pred = model.predict(img)[0]
+    print(pred)
+    predicted_class_index = np.argmax(pred)
+    print(predicted_class_index)
+    predicted_class_name = class_names[predicted_class_index]
+
+    return predicted_class_name
+
+# print(predict_skin_disease(r"D:\Downloads\contest\SIH\Data Set Skin Disease\train\Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions\actinic-cheilitis-sq-cell-lip-1.jpg"))
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def greet():
+    return "Hello World"
 
-
-@app.get('/favicon.ico', include_in_schema=False)
-async def favicon():
-    return FileResponse('favicon.ico')
-
-
-@app.get("/item/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/items/")
-async def list_items():
-    return [{"item_id": 1, "name": "Foo"}, {"item_id": 2, "name": "Bar"}]
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+@app.post("/predict")
+def predict(image_path) :
+    return predict_skin_disease(image_path)
